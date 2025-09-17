dbdb82d66741af20015288ffe636bd7f3eaff8f3
vidhusan12 <vidhusan709@gmail.com>
2025-09-18 02:05:00 +1200

    add coin and shadow images with classes

--- logic.js
+++ logic.js
@@ -1,7 +1,13 @@
 
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('.flip-btn').addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('.flip-btn').addEventListener('click', function () {
     const result = Math.random() < 0.5 ? "Heads" : "Tails";
+    if (result === "Heads") {
+      document.querySelector('.coin-img').src = "resources/heads.svg"
+    } else
+      document.querySelector('.coin-img').src = "resources/tails.svg"
+    //logs if its head or tail to the page
+    document.querySelector('.coin-result').textContent = result;
     console.log(result);
   });
 });
